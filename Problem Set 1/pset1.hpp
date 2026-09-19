073397962d80b820f2c1a501ddae787213743a8c
Eric Ge <ege@college.harvard.edu>
2026-09-19 14:58:36 -0400

    Finish rename

--- Problem Set 1/pset1.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void inner_product_mmm(float *C, float const *A, float const *B, int M, int N, int K);
-void outer_product_mmm(float *C, float const *A, float const *B, int M, int N, int K);
